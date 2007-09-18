1017075ac08e51f1d5ceed58b41afd03008c36eb
popeeaco <unknown>
2007-09-18 01:58:28 +0000

    i

--- ImpAST.hs
+++ ImpAST.hs
@@ -402,6 +402,7 @@ instance ShowImpp MethDecl where
     "{-\nOK:="++showSet (getOKOutcome (methOut m)) ++ "\n" ++
     "individualERRs:={"++showImpp (methErrs m)++"}\n"++
     "ERR:="++showSet (getERROutcome (methOut m)) ++ "\n-}\n" ++
+-- printing NEVER/MUST/MAY is expensive. For benchmark evaluation is disabled:
 --      "NEVER_BUG:="++showSet (snd((methOutBugs m)!!0)) ++ "\n" ++
 --      "MUST_BUG:="++showSet (snd((methOutBugs m)!!1)) ++ "\n" ++
 --      "MAY_BUG:="++showSet (snd((methOutBugs m)!!2)) ++ "\n" ++
