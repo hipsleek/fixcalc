3094de1448254dd95dba47c26677d43513bf9435
popeeaco <unknown>
2008-05-10 04:36:57 +0000

    i

--- ImpConfig.hs
+++ ImpConfig.hs
@@ -21,42 +21,42 @@ type FixFlags = (Int,Heur)
 data Heur = SimilarityHeur | DifferenceHeur | HausdorffHeur | SimInteractiveHeur deriving (Show,Eq)
 
 data Flags = Flags {
-  isIndirectionIntArray:: Bool, -- ^Collect constraints regarding Min and Max values of the array elements. Default is False.
-  checkingAfterInference:: Bool,
   noInference:: Bool,
+  checkingAfterInference:: Bool,
+  isIndirectionIntArray:: Bool, -- ^Collect constraints regarding Min and Max values of the array elements. Default is False.
+  outputFile:: String,
+  showDebugMSG:: Int ,
+{- | 0 -> do not show any fixpoint messages
+     1 -> show only loss-of-precision messages
+     2 -> show loss-of-precision and hull-widening messages -}
+  prederivation:: Prederivation, -- ^Kind of prederivation. Default is PostPD.
+  postcondition:: Postcondition, -- ^Whether to accumulate preconditions in the computed postcondition. Default is True.
+  traceIndividualErrors:: Bool,  -- ^Trace individual errors for Dual Analysis.
+  fixFlags:: FixFlags, -- ^Number of disjuncts (m) and heuristic function to compute disjunct affinity. Default is (5, Similarity).
+  whatHull:: Hull, -- ^What least upper bound operator: Hull or ConvexHull. Default is Hull.
 ---- derive 2 stronger preconditions that need specialization for recursive functions
 ---- otherwise the resulting program may not type-check
   separateFstFromRec:: Bool,
   useAnnotatedFixpoint:: Bool, -- ^Use the annotated fixpoint where it is provided. Default is False.
   useSelectiveHull:: Bool, -- Old by the old fixpoint. Quicksort (Hanoi and Mergesort) require selectiveHull for precise result.
-  widenEarly:: Bool, -- ^Used by the old fixpoint. Quicksort requires widenEarly for precise result.
-  fixFlags:: FixFlags, -- ^Number of disjuncts (m) and heuristic function to compute disjunct affinity. Default is (5, Similarity).
-  prederivation:: Prederivation, -- ^Kind of prederivation. Default is PostPD.
-  postcondition:: Postcondition, -- ^Whether to accumulate preconditions in the computed postcondition. Default is True.
-  traceIndividualErrors:: Bool,  -- ^Trace individual errors for Dual Analysis.
-  whatHull:: Hull, -- ^What least upper bound operator: Hull or ConvexHull. Default is Hull.
-  showDebugMSG:: Int ,
-{- | 0 -> do not show any fixpoint messages
-     1 -> show only loss-of-precision messages
-     2 -> show loss-of-precision and hull/widening messages -}
-  outputFile:: String
+  widenEarly:: Bool -- ^Used by the old fixpoint. Quicksort requires widenEarly for precise result.
 } deriving Show
 
 defaultFlags = Flags { 
-  isIndirectionIntArray = False,
-  checkingAfterInference = False,
   noInference = False,
-  separateFstFromRec = False,
-  useAnnotatedFixpoint = True, 
-  useSelectiveHull = False,
-  widenEarly = True, 
-  fixFlags = (5,SimilarityHeur),
+  checkingAfterInference = False,
+  isIndirectionIntArray = False,
+  outputFile = "a",
+  showDebugMSG = 0,
   prederivation = PostPD,
   postcondition = StrongPost, 
   traceIndividualErrors = False,
+  fixFlags = (5,SimilarityHeur),
   whatHull = Hull,
-  showDebugMSG = 0,
-  outputFile = "a"
+  separateFstFromRec = False,
+  useAnnotatedFixpoint = True, 
+  useSelectiveHull = False,
+  widenEarly = True
 }
 
 
--- ImpMain.hs
+++ ImpMain.hs
@@ -76,23 +76,27 @@ processCmdLine cmdLine =
 
 showHelpMessage = 
   putStrLn "Usage: imp file [options]" >>
-  putStrLn "Options:" >>
+  putStrLn "\nGeneral options:" >>
+  putStrLn "  +infer -check\t\t Infer the input file and output the result in *.impi file." >>
+  putStrLn "  +infer +check\t\t Infer the input file and type-check the result." >>
+  putStrLn "  -infer +check\t\t Type-check the input file (written in impt format)." >>
+  putStrLn "  +indir\t\t Enable array indirection." >>
+  putStrLn "  -o:<file>\t\t Place the output in <file.impi>." >>
+  putStrLn "  -v:<level>\t\t Be verbose, where <level> is the verbosity level (0, 1 or 2)." >>
+  putStrLn "   0\t\t\t Do not show any fixpoint messages." >>
+  putStrLn "   1\t\t\t Show only loss-of-precision messages." >>
+  putStrLn "   2\t\t\t Show loss-of-precision and hull/widening messages." >>
+  putStrLn "\nPre/Postcondition derivation options:" >>
+  putStrLn "  <pre><post>\t\t Use the <pre><post> combination of prederivation and postcondition. <pre> can be Post/Strong/Sel/Weak. <post> can be Strong/Weak." >>
+  putStrLn "  +individual\t\t Enable tracing of individual bug conditions." >>
+  putStrLn "\nFixpoint options:" >>
   putStrLn "  -m:<bound>\t\t Use <bound>-disjunctive fixpoint, where <bound> is the maximum number of disjuncts." >>
   putStrLn "  -club:<lub>\t\t Use the conjunctive lub operator <lub>, where <lub> can be Hull or ConvexHull." >>
   putStrLn "  -dlub:<lub>\t\t Use the disjunctive lub operator <lub>, where <lub> can be Similarity, Hausdorff or Interactive." >>
   putStrLn "   Similarity\t\t Use the Planar affinity-based heuristic [ASIAN'06]." >>
   putStrLn "   Hausdorff\t\t Use the Hausdorff-based heuristic [Sriram et al-SAS'06]." >>
   putStrLn "   Interactive\t\t Allow user to specify interactively which disjuncts to combine, and revert to Similarity-based heuristic when unspecified." >>
---  putStrLn "  <pre><post>\t Use the <pre><post> combination of prederivation and postcondition. <pre> can be Post/Strong/Sel/Weak. <post> can be Strong/Weak." >>
---  putStrLn "  +indir\t Enable array indirection." >>
---  putStrLn "  +check\t Infer the input file and type-check the result." >>
-  putStrLn "  -infer +check\t\t Type-check the input file (written in impt format)." >>
-  putStrLn "  +individual\t\t Enable tracing of individual bug conditions." >>
-  putStrLn "  -v:<level>\t\t Be verbose, where <level> is the verbosity level (0, 1 or 2)." >>
-  putStrLn "   0\t\t\t Do not show any fixpoint messages." >>
-  putStrLn "   1\t\t\t Show only loss-of-precision messages." >>
-  putStrLn "   2\t\t\t Show loss-of-precision and hull/widening messages." >>
-  putStrLn "Default arguments: -m:5 -club:Hull -dlub:Similarity -individual -v:0" >>
+  putStrLn "Default arguments: +infer -check -individual -v:0 -m:5 -club:Hull -dlub:Similarity" >>
   return Nothing
 
 allArgs:: Flags -> [String] -> IO (Maybe Flags)
@@ -105,19 +109,19 @@ allArgs flags (a:args) =
 
 oneArg:: Flags -> String -> IO (Maybe Flags)
 oneArg prevFs arg = case arg of
-  "-indir" ->       return $ Just prevFs{isIndirectionIntArray=False}
-  "+indir" ->       return $ Just prevFs{isIndirectionIntArray=True}
+-- General options:
+  "+infer" ->       return $ Just prevFs{noInference=False}
+  "-infer" ->       return $ Just prevFs{noInference=True}
   "-check" ->       return $ Just prevFs{checkingAfterInference=False} 
   "+check" ->       return $ Just prevFs{checkingAfterInference=True}
-  "-sep" ->         return $ Just prevFs{separateFstFromRec=False}
-  "+sep" ->         return $ Just prevFs{separateFstFromRec=True}
-  "-anno" ->        return $ Just prevFs{useAnnotatedFixpoint=False}
-  "+anno" ->        return $ Just prevFs{useAnnotatedFixpoint=True}
-  "-selHull" ->     return $ Just prevFs{useSelectiveHull=False}
-  "+selHull" ->     return $ Just prevFs{useSelectiveHull=True}
-  "-widenEarly" ->  return $ Just prevFs{widenEarly=False}
-  "+widenEarly" ->  return $ Just prevFs{widenEarly=True}
-  '-':'m':':':m ->  return $ Just prevFs{fixFlags=(read m,snd (fixFlags prevFs))}
+  "-indir" ->       return $ Just prevFs{isIndirectionIntArray=False}
+  "+indir" ->       return $ Just prevFs{isIndirectionIntArray=True}
+  '-':'o':':':file -> return $ Just prevFs{outputFile=file}
+  '-':'v':':':level -> 
+                    case snd (fixFlags prevFs) of
+                      SimInteractiveHeur -> return $ Just prevFs{showDebugMSG=2}
+                      _ -> return $ Just prevFs{showDebugMSG=read level}
+-- Pre/Postcondition derivation options:
   "PostStrong" ->   return $ Just prevFs{prederivation=PostPD,postcondition=StrongPost}
   "PostWeak" ->     return $ Just prevFs{prederivation=PostPD,postcondition=WeakPost}
   "StrongStrong" -> return $ Just prevFs{prederivation=StrongPD,postcondition=StrongPost}
@@ -128,17 +132,23 @@ oneArg prevFs arg = case arg of
   "WeakWeak" ->     return $ Just prevFs{prederivation=WeakPD,postcondition=WeakPost}
   "+individual" ->  return $ Just prevFs{traceIndividualErrors=True}
   "-individual" ->  return $ Just prevFs{traceIndividualErrors=False}
-  "-infer" ->       return $ Just prevFs{noInference=True}
+-- Fixpoint options:
+  '-':'m':':':m ->  return $ Just prevFs{fixFlags=(read m,snd (fixFlags prevFs))}
   "-club:Hull" ->   return $ Just prevFs{whatHull=Hull}
   "-club:ConvexHull" -> return $ Just prevFs{whatHull=ConvexHull}
   "-dlub:Similarity" -> return $ Just prevFs{fixFlags=(fst (fixFlags prevFs),SimilarityHeur)}
   "-dlub:Hausdorff" -> return $ Just prevFs{fixFlags=(fst (fixFlags prevFs),HausdorffHeur)} 
   "-dlub:Interactive" -> return $ Just prevFs{fixFlags=(fst (fixFlags prevFs),SimInteractiveHeur),showDebugMSG=2} 
-  '-':'v':':':level -> 
-                    case snd (fixFlags prevFs) of
-                      SimInteractiveHeur -> return $ Just prevFs{showDebugMSG=2}
-                      _ -> return $ Just prevFs{showDebugMSG=read level}
-  '-':'o':':':file -> return $ Just prevFs{outputFile=file}
+-- Deprecated options:
+  "-sep" ->         return $ Just prevFs{separateFstFromRec=False}
+  "+sep" ->         return $ Just prevFs{separateFstFromRec=True}
+  "-anno" ->        return $ Just prevFs{useAnnotatedFixpoint=False}
+  "+anno" ->        return $ Just prevFs{useAnnotatedFixpoint=True}
+  "-selHull" ->     return $ Just prevFs{useSelectiveHull=False}
+  "+selHull" ->     return $ Just prevFs{useSelectiveHull=True}
+  "-widenEarly" ->  return $ Just prevFs{widenEarly=False}
+  "+widenEarly" ->  return $ Just prevFs{widenEarly=True}
+
   _ -> 
     putStrLn ("imp: unrecognised flag: " ++ arg) >>
     showHelpMessage
